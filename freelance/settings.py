aa36599fab87f2f9a15fda6c09061613a56ecb75
Arthur Atrokhov <aatrokhov@gmail.com>
2018-08-01 14:50:51 +0600

    Changed db to PostgreSQL

--- freelance/settings.py
+++ freelance/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'freelance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'freelance_db',
+        'USER' : 'postgres',
+        'PASSWORD' : 'proofYou3',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
